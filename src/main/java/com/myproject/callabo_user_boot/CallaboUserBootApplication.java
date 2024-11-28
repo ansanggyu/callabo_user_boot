5f6eb1e3743481c2ddd97aff23dd78e6b1de83ba
jijihorang <jiho7213@gmail.com>
2024-11-28 13:49:13 +0900

    CJH callabo_user_boot 프로젝트 생성 (2024-11-28)

---
+++ src/main/java/com/myproject/callabo_user_boot/CallaboUserBootApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.callabo_user_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallaboUserBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallaboUserBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myproject/callabo_user_boot/CallaboUserBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.callabo_user_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallaboUserBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
