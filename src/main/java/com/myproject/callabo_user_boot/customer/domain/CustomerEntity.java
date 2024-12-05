cc444b1af4fe86942cc6cd814e35c242564dd0f4
SangGyu Ahn <gd20624@gmail.com>
2024-12-05 10:45:14 +0900

    credentials를 .env파일에 변환

--- src/main/java/com/myproject/callabo_user_boot/customer/domain/CustomerEntity.java
+++ src/main/java/com/myproject/callabo_user_boot/customer/domain/CustomerEntity.java
@@ -2,7 +2,7 @@ package com.myproject.callabo_user_boot.customer.domain;
 
 import com.myproject.callabo_user_boot.common.BasicEntity;
 import jakarta.persistence.*;
-import lombok.Getter;
+import lombok.*;
 
 @Entity
 @Table(name = "customer")
