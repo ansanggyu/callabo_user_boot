8924b499296d4a6938d0b07416c0157dfe74134c
SangGyu Ahn <gd20624@gmail.com>
2024-12-05 10:27:43 +0900

    test commit

--- src/main/java/com/myproject/callabo_user_boot/customer/domain/CustomerEntity.java
+++ src/main/java/com/myproject/callabo_user_boot/customer/domain/CustomerEntity.java
@@ -2,10 +2,10 @@ package com.myproject.callabo_user_boot.customer.domain;
 
 import com.myproject.callabo_user_boot.common.BasicEntity;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
 
 @Entity
-@Data
+@Getter
 @Table(name = "customer")
 public class CustomerEntity extends BasicEntity {
 
